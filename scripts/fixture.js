733b7f2871764a1b6470f9e5173ae06a4d708a3e
ryota-murakami <dojce1048@gmail.com>
2022-11-11 19:50:20 +0900

    add fixture.js

---
+++ scripts/fixture.js
@@ -0,0 +1,36 @@
+var fs = require('fs')
+var os = require('os')
+
+var faker = require('faker')
+
+function writeJson(fileName, object) {
+  if (Array.isArray(object)) {
+    const obj = { [fileName]: object }
+    fs.writeFileSync(
+      fileName + '.json',
+      JSON.stringify(obj, null, 2).replace(/\n/g, os.EOL) + os.EOL
+    )
+    return
+  }
+
+  RegExp.prototype.toJSON = RegExp.prototype.toString
+  fs.writeFileSync(
+    fileName + '.json',
+    JSON.stringify(object, null, 2).replace(/\n/g, os.EOL) + os.EOL
+  )
+}
+
+var userList = []
+
+// @TODO take from parameter
+for (i = 0; i <= 1; i++) {
+  userList.push({
+    card: faker.helpers.createCard(),
+    email: faker.internet.email(),
+    name: faker.name.findName(),
+  })
+}
+// @TODO take from parameter
+writeJson('userList', userList)
+
+process.exit(0)
