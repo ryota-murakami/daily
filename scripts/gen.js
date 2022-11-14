9e5d0e879a502099961fbc9b0a2eee39b82d0bb1
ryota-murakami <dojce1048@gmail.com>
2022-11-14 10:12:48 +0900

    create gen scripts

--- scripts/gen.js
+++ scripts/gen.js
@@ -1,4 +1,7 @@
 #!/usr/bin/env node
+import fs from 'fs'
+import process from 'process'
+import path from 'path'
 
 import meow from 'meow'
 
@@ -21,13 +24,21 @@ const cli = meow(
 )
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-function genTodayFile(input, flags) {
-  // @TODO set project root path in variable
-  // @TODO get Local Time
-  // @TODO format datetime style
-  // @TODO create year(2022) directory if it is not exist
-  // @TODO create month(11) directory if it is not exist
-  // @TODO write file that named 2022_Fri_Nov_11.js
+function genTodaysFile(input, flags) {
+  const currentDir = process.cwd()
+  const date = new Date()
+  const year = date.toLocaleDateString(undefined, {year: 'numeric'})
+  const month = {string: date.toLocaleDateString(undefined, {month: 'short'}), number: date.toLocaleDateString(undefined, {month: 'numeric'}).toString().padStart('2', '0')}
+  const day = date.toLocaleDateString(undefined, {day: 'numeric'}).toString().padStart('2', '0')
+  const weekday = date.toLocaleDateString(undefined, {weekday: 'short' })
+  const sourceDir = path.join(currentDir, year, month.number)
+  // create 2040/04 folder
+  fs.mkdirSync(sourceDir, {recursive: true})
+  
+  const fileName = `${year}_${weekday}_${month.string}_${day}.js`
+  const filePath = path.join(sourceDir, fileName)
+
+  if (!fs.existsSync(filePath)) fs.writeFileSync(filePath, '')
 }
 
-genTodayFile(cli.input, cli.flags)
+genTodaysFile(cli.input, cli.flags)
